36fb4d460b64609a0a88d0e4af16384b2d254577
kumarChandan <145843679+KumarChandan23@users.noreply.github.com>
2024-12-21 19:46:28 +0000

    variable and datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,5 @@
+const accountId = 1225533;
+let accountEmail = "kumar@gmail.com";
+var accountCity = "1243"
+// prefer not to use var because of issue in block scop and function scope
+console.table([accountId, accountEmail, accountCity]);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer version code
+// alert("click") // we are using node js not browser, so it will not work
+
+console.log(3 + 3);
+
+// number = 2 to power 53
+// bitint = for big number used in trading
+// string = "", ''
+// boolean = true / false
+// null = standalone value and it is object when you console it you will get object type
+// undefined = not defined while initialization and is type when you console it you will get undefined type
+// symbol = to find the unique value
